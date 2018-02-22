04b7e3281647d667447f360fd59c7326b64a610d
Mike Frisch <mfrisch@univa.com>
2018-02-21 21:14:32 -0500

    Added unit test table for Sum

--- main_test.go
+++ main_test.go
@@ -15,3 +15,23 @@ func TestSubtract(t *testing.T) {
 		t.Errorf("Quotient was incorrect, got: %d, want: %d.", total, 5)
 	}
 }
+
+func MultiTestSum(t *testing.T) {
+	tables := []struct {
+		x int
+		y int
+		n int
+	}{
+		{1, 1, 2},
+		{1, 2, 3},
+		{2, 2, 4},
+		{5, 2, 7},
+	}
+
+	for _, table := range tables {
+		total := Sum(table.x, table.y)
+		if total != table.n {
+			t.Errorf("Sum of (%d+%d) was incorrect, got: %d, want: %d.", table.x, table.y, total, table.n)
+		}
+	}
+}
