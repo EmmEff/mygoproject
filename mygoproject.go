d4ef9908fe992e8ed69b5504fb4d0893fddaed36
Mike Frisch <mfrisch@univa.com>
2018-02-21 21:14:18 -0500

    Added func comments

--- mygoproject.go
+++ mygoproject.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
+// Subtract subtracts y from x
 func Subtract(x int, y int) int {
 	return x - y
 }
 
+// Sum adds x and y
 func Sum(x int, y int) int {
 	return x + y
 }
